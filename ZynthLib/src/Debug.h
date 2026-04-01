799a85aa44aa9e660028a27f0e9f1b52141e37a0
Mark Ebersole <markebby@gmail.com>
2026-04-01 00:17:36 -0500

    Removed path from debug prints and only display file name.

--- ZynthLib/src/Debug.h
+++ ZynthLib/src/Debug.h
@@ -23,13 +23,13 @@ void ErrorMsg  (const char* label, const char* func, const char* const fmt, ...)
 char* ErrorStringI2C (int err);
 void  BootDebug      (void);
 
-#define PAUSE   {printf("--- %s:%d\n",__FILE__,__LINE__);while(!Serial.available ()) continue;char s=Serial.read();}
-#define DbgD(d) {printf("==> %s:%d %s = %d\n",__FILE__,__LINE__, #d, d);}
-#define DbgDn(d) {printf("==> %s:%d %s = %d\n\n",__FILE__,__LINE__, #d, d);}
-#define DbgX(x) {printf("==> %s:%d %s = 0x%X\n",__FILE__,__LINE__, #x, x);}
-#define DbgF(f) {printf("==> %s:%d %s = %f\n",__FILE__,__LINE__, #f, f);}
-#define DbgS(s) {printf("==> %s:%d %s = %s\n",__FILE__,__LINE__, #s, s);}
-#define DbgSt(s) {printf("==> %s:%d %s = %s\n",__FILE__,__LINE__, #s, s.c_str());}
+#define PAUSE   {printf("--- %s:%d\n",__FILE_NAME__,__LINE__);while(!Serial.available ()) continue;char s=Serial.read();}
+#define DbgD(d) {printf("==> %s:%d %s = %d\n",__FILE_NAME__,__LINE__, #d, d);}
+#define DbgDn(d) {printf("==> %s:%d %s = %d\n\n",__FILE_NAME__,__LINE__, #d, d);}
+#define DbgX(x) {printf("==> %s:%d %s = 0x%X\n",__FILE_NAME__,__LINE__, #x, x);}
+#define DbgF(f) {printf("==> %s:%d %s = %f\n",__FILE_NAME__,__LINE__, #f, f);}
+#define DbgS(s) {printf("==> %s:%d %s = %s\n",__FILE_NAME__,__LINE__, #s, s);}
+#define DbgSt(s) {printf("==> %s:%d %s = %s\n",__FILE_NAME__,__LINE__, #s, s.c_str());}
 
 #define DbgN    {printf("\n");}
 
